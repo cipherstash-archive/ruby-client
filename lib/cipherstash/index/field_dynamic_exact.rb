bc2e3834bb7d8b23d5eea13f0c7aa681c68c5db7
Matt Palmer <matt@cipherstash.com>
2022-05-02 09:53:44 +1000

    Experimental implementation of a field-dynamic-exact index type

---
+++ lib/cipherstash/index/field_dynamic_exact.rb
@@ -0,0 +1,29 @@
+module CipherStash
+  class Index
+    # Implementation for the 'field-dynamic-exact' index type
+    #
+    # @private
+    class FieldDynamicExact < Index
+      INDEX_OPS = {
+        "eq" => -> (idx, f, s) do
+          id = UUIDHelpers.blob_from_uuid(idx.id)
+          [{ indexId: id, exact: { term: [idx.ore_encrypt("#{f}:#{s}").to_s] } }]
+        end,
+      }
+
+      def analyze(uuid, record)
+        blid = blob_from_uuid(uuid)
+
+        raw_terms = collect_string_fields(record)
+
+        if raw_terms.all?(&:nil?)
+          return nil
+        end
+
+        terms = raw_terms.map { |f, s| "#{f}:#{s}" }
+
+        { indexId: blob_from_uuid(@id), terms: terms.map { |t| { term: [ore_encrypt(t).to_s], link: blid } } }
+      end
+    end
+  end
+end
