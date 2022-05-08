035eb3d244e84408f62e917342ecb99a30ca3e10
Matt Palmer <matt@cipherstash.com>
2022-05-09 08:41:49 +1000

    Remove duplicate terms from indexes

--- lib/cipherstash/index/dynamic_match.rb
+++ lib/cipherstash/index/dynamic_match.rb
@@ -22,7 +22,7 @@ module CipherStash
         if raw_terms == []
           nil
         else
-          terms = raw_terms.map { |s| text_processor.perform(s) }.flatten
+          terms = raw_terms.map { |s| text_processor.perform(s) }.flatten.uniq
           { indexId: blob_from_uuid(@id), terms: terms.map { |t| { term: [ore_encrypt(t).to_s], link: blid } } }
         end
       end
