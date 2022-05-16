49d1902bf0f9c14350e174930662ad49c922b8ee
Matt Palmer <matt@cipherstash.com>
2022-05-16 12:45:50 +1000

    Stop using Aws::AssumeRoleWebIdentityCredentials
    I've never been particularly keen on that class' need to read an access token
    off disk; it seems like an unnecessary security risk to leave a token laying
    around if it doesn't have to, and it complicates matters for people who don't have
    an explicit profile set (because we needed to write that file *somewhere*).
    
    It's only 20 lines of code to do it ourselves, so we'll do that instead.

---
+++ lib/cipherstash/client/aws_federated_credentials.rb
@@ -0,0 +1,44 @@
+require "aws-sdk-core"
+require "aws-sdk-sts"
+
+module CipherStash
+  class Client
+    # Responsible for retrieving AWS credentials via federation (AssumeRoleWithWebIdentity)
+    #
+    # @private
+    class AwsFederatedCredentials
+      EXPIRY_GRACE_PERIOD_SECONDS = 120
+
+      def initialize(profile, role_arn, region, logger)
+        @profile, @role_arn, @region, @logger = profile, role_arn, region, logger
+
+        @expires_at = nil
+      end
+
+      def credentials
+        res = sts_client.assume_role_with_web_identity(
+          role_arn: @role_arn,
+          role_session_name: "ruby-client",
+          web_identity_token: @profile.with_access_token[:access_token]
+        )
+
+        @expires_at = res.credentials.expiration
+
+        @logger.debug("AwsFederatedCredentials#fresh_credentials") { "returning creds; new expiry #{@expires_at}" }
+
+        Aws::Credentials.new(res.credentials.access_key_id, res.credentials.secret_access_key, res.credentials.session_token)
+      end
+
+      def expired?
+        @logger.debug("AwsFederatedCredentials#expired?") { "Expiry check; @expires_at=#{@expires_at.inspect}" }
+        @expires_at.nil? || (Time.now + EXPIRY_GRACE_PERIOD_SECONDS > @expires_at)
+      end
+
+      private
+
+      def sts_client
+        @sts_client ||= Aws::STS::Client.new(region: @region)
+      end
+    end
+  end
+end
