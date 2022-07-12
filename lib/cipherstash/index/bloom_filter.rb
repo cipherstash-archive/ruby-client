a391cb28d0f0a0cf574d2d35136f33e517f41648
Drew Thomas <drew@cipherstash.com>
2022-07-12 15:34:02 +1000

    Add docs for bloom filter changes

--- lib/cipherstash/index/bloom_filter.rb
+++ lib/cipherstash/index/bloom_filter.rb
@@ -3,6 +3,9 @@ require_relative "../client/error"
 
 module CipherStash
   class Index
+    # A bloom filter implementation designed to be used with the *FilterMatch index classes.
+    #
+    # @private
     class BloomFilter
       FILTER_TERM_BITS_MIN = 3
       FILTER_TERM_BITS_MAX = 16
@@ -10,13 +13,32 @@ module CipherStash
       FILTER_SIZE_DEFAULT = 256
       VALID_FILTER_SIZES = [32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536]
 
-      private_constant :FILTER_TERM_BITS_MIN, :FILTER_TERM_BITS_MAX, :FILTER_TERM_BITS_DEFAULT,
-        :FILTER_SIZE_DEFAULT, :VALID_FILTER_SIZES
-
+      # The "set" bits of the bloom filter
       attr_reader :bits
+
+      # The size of the bloom filter in bits. Commonly referred to as "m". Since we only keep track of the set bits, the filter
+      # size determines the maximum value of the positions stored in the bits attr.
+      #
+      # Valid values are powers of 2 from 32 to 65536.
+      #
+      # @return [Integer]
       attr_reader :filter_size
+
+      # The number of bits to set per term. Commonly referred to as "k".
+      #
+      # Valid values are integers from 3 to 16.
+      #
+      # @return [Integer]
       attr_reader :filter_term_bits
 
+      # Creates a new bloom filter with the given key and filter match index settings.
+      #
+      # @param key [String] the key to use for hashing terms.
+      #
+      # @param opts [Hash] the index settings.
+      #   "filterSize" and "filterTermBits" are used to set the filter_size and filter_term_bits attrs.
+      #
+      # @raise [CipherStash::Client::Error::InvalidSchemaError] if an invalid "filterSize" or "filterTermBits" is given.
       def initialize(key, opts = {})
         @key = key
         @bits = Set.new()
@@ -34,6 +56,11 @@ module CipherStash
         end
       end
 
+      # Adds the given term to the bloom filter and returns the filter instance.
+      #
+      # @param term [String] the term to add
+      #
+      # @return [CipherStash::Index::BloomFilter]
       def add(term)
         hash = OpenSSL::HMAC.digest("SHA256", @key, term)
 
@@ -46,6 +73,13 @@ module CipherStash
         self
       end
 
+      # Returns true if the bloom filter is a subset of the other bloom filter and returns false otherwise.
+      #
+      # Can give false positives.
+      #
+      # @param other [CipherStash::Index::BloomFilter] the other bloom filter to check against.
+      #
+      # @return [Boolean]
       def subset?(other)
         @bits.subset?(other.bits)
       end
