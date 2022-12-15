373dd3d2c9dacdd7fd1166c2faaca8284f5c920f
fimac <fiona@cipherstash.com>
2022-12-15 15:04:39 +1100

    update text processor specs

--- spec/cipherstash/analysis/text_processor_spec.rb
+++ spec/cipherstash/analysis/text_processor_spec.rb
@@ -1,4 +1,5 @@
 require 'cipherstash/analysis/text_processor'
+require "cipherstash/client"
 
 RSpec.describe CipherStash::Analysis::TextProcessor do
   describe "Standard text processor" do
@@ -17,6 +18,44 @@ RSpec.describe CipherStash::Analysis::TextProcessor do
   end
 
   describe "Standard text processor with an ngram filter" do
+    it "raises an error if min length and max length are not provided" do
+      expect {
+        CipherStash::Analysis::TextProcessor.new({
+          "tokenFilters" => [
+            { "kind" => "downcase" },
+            { "kind" => "ngram" }
+          ],
+          "tokenizer" => { "kind" => "standard" }
+        })
+      }.to raise_error(CipherStash::Client::Error::InternalError, "Min length and max length not provided with ngram filter. Please specify ngram token length using '{kind: :ngram, min_length: 3, max_length: 8}'")
+    end
+
+    ["1", { foo: "bar" }, Object.new].each do |length|
+      it "raises an error if invalid length of #{length.inspect} provided" do
+        expect {
+          CipherStash::Analysis::TextProcessor.new({
+            "tokenFilters" => [
+              { "kind" => "downcase" },
+              { "kind" => "ngram", "minLength" => length, "maxLength" => length }
+              ],
+            "tokenizer" => { "kind" => "standard" }
+          })
+        }.to raise_error(CipherStash::Client::Error::InternalError, "The values provided to the min and max length must be of type Integer.")
+      end
+    end
+
+    it "raises an error if the min length is greater than the max length" do
+      expect {
+        CipherStash::Analysis::TextProcessor.new({
+          "tokenFilters" => [
+            { "kind" => "downcase" },
+            { "kind" => "ngram", "minLength" => 4, "maxLength" => 3 }
+          ],
+          "tokenizer" => { "kind" => "standard" }
+        })
+        }.to raise_error(CipherStash::Client::Error::InternalError, "The ngram filter min length must be less than or equal to the max length")
+    end
+
     it "splits text into ngrams using min length of 3 and max length of 8" do
       tokenizer =
         CipherStash::Analysis::TextProcessor.new({
